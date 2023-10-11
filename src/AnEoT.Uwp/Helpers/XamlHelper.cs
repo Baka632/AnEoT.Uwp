c5605b64f9f70f24bcd54e3f954ccca52a1e9086
Baka632 <stevemc123456@outlook.com>
2023-10-11 22:57:10 +0800

    修复XamlHelper中的逻辑bug，应用名中的全角符号改为半角

--- src/AnEoT.Uwp/Helpers/XamlHelper.cs
+++ src/AnEoT.Uwp/Helpers/XamlHelper.cs
@@ -22,12 +22,21 @@ public static class XamlHelper
         };
     }
     
+    public static Visibility ToVisibility(bool value)
+    {
+        return value switch
+        {
+            true => Visibility.Visible,
+            false => Visibility.Collapsed
+        };
+    }
+    
     public static Visibility ToVisibility(bool? value)
     {
         return value switch
         {
-            true => Visibility.Collapsed,
-            false => Visibility.Visible,
+            true => Visibility.Visible,
+            false => Visibility.Collapsed,
             null => Visibility.Collapsed
         };
     }
@@ -36,8 +45,8 @@ public static class XamlHelper
     {
         return value switch
         {
-            true => Visibility.Collapsed,
-            false => Visibility.Visible,
+            true => Visibility.Visible,
+            false => Visibility.Collapsed,
             null => defaultVisibilityForNull
         };
     }
